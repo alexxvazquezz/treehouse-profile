eb85826ce81634bbb41aff9e46fbca23354f1ede
Alejandro Vazquez <alexv@alejandros-mbp.home>
2017-12-26 13:31:34 -0800

    First Push

---
+++ app.js
@@ -0,0 +1,10 @@
+// Problem: We need a simple way to look at a users badge Count and points from a web browser
+// Solution: Use Node.js to perform the profile looks ups and serve our templates via HTTP
+
+//1. Create a web server
+
+//2. .Handle HTTP route GET / and POST / i.e. Home
+
+//3. Handle HTTP route GET /:username i.e /username
+
+//4. Function that handles the reading of files and merge in value
